029d7eb121dbe8e3033aad2ff3a9945e81d539bd
Sangeun Lee <goakgoak1995@gmail.com>
2019-12-05 15:12:36 +0900

    [list 1-3] implementation ScreenUI for  menu1, menu2

---
+++ example/part1/Menu1ScreenUI.java
@@ -0,0 +1,8 @@
+public class Menu1ScreenUI implements ScreenUI {
+    public void show(){
+        System.out.println("메뉴 1 화면으로 전환");
+    }
+    public void handleButton1Click(){
+         System.out.println("메뉴 1 화면의 버튼 처리");
+    }
+}
---
+++ example/part1/Menu2ScreenUI.java
@@ -0,0 +1,8 @@
+public class Menu2ScreenUI implements ScreenUI {
+    public void show(){
+        System.out.println("메뉴 2 화면으로 전환");
+    }
+    public void handleButton1Click(){
+         System.out.println("메뉴 2 화면의 버튼 처리");
+    }
+}
