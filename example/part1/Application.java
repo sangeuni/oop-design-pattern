9f704ea50929cee5011deefab7c3caa05419f0bb
Sangeun Lee <goakgoak1995@gmail.com>
2019-12-05 15:24:52 +0900

    [list 1-8] Update Application.java
    add menu3 related code

--- example/part1/Application.java
+++ example/part1/Application.java
@@ -1,30 +1,44 @@
-public class Application implements OnClickedListener{
+public class Application {
     
     private Menu menu1 = new Menu("menu1");
     private Menu menu2 = new Menu("menu2");
+    // 새로 추가된 부분
+    private Menu menu2 = new Menu("menu3");
     private Button button1 = new Button("button1");
     
     private ScreenUI currentScreen = null;
     
-    public Application() {
-        menu1.setOnClickListener(this);
-        menu2.setOnClickListener(this);
-        button1.setOnClickListener(this);
+    public Application(){
+        menu1.setOnClickListener(menuListner);
+        menu2.setOnClickListener(menuListner);
+        // 새로 추가된 부분
+        menu3.setOnClickListener(menuListner);
+        button1.setOnClickListener(buttonListner);
     }
     
-    public void clicked(Component eventSource) {
-        String sourceId = eventSource.getId();
-        if(sourceId.equals("menu1")){
-            currentScreen = new Menu1ScreenUI();
+    private OnClickListener menuListener = new OnClickListener(){
+        public void clicked()(Componenet eventSource){
+            String sourceId = eventSource,getId();
+            if(sourceId.equals("menu1")){
+                currentScreen = new Menu1ScreenUI();
+            } else if(sourceId.equals("menu2")){
+                currentScreen = new Menu2ScreenUI();
+            } // 새로 추가된 부분 
+            else if(sourceId.equals("menu3")){
+                currentScreen = new Menu3ScreenUI();
+            }
             currentScreen.show();
-        } else if(sourceId.equals("menu2")){
-           currentScreen = new Menu2ScreenUI();
-            currentScreen.show();
-        } else if(sourceId.equals("button1")){
-            if(currentScreen == null)
-                return;
-            // 메뉴1 인지 메뉴2 인지에 상관없이 currentScreen의 메서드를 실행
-            currentScreen.handleButton1Click();
-        }  
-    }
+        }
+    };
+    
+    private OnClickListener buttonListener = new OnClickListener(){
+         public void clicked()(Componenet eventSource){
+             if(currentScreen == null)
+                 return;
+             String sourceId = eventSource.getId();
+             if(sourceId.equals("button1")){
+                 currentScreen.handleButton1Click();
+             }
+         }
+    };
 }
