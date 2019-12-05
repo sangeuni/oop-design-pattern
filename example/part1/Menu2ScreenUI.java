752b064667683efd3e2797bc40fbb2a4f21e0f81
Sangeun Lee <goakgoak1995@gmail.com>
2019-12-05 15:19:05 +0900

    [list 1-6] implementation handleButton2Click() method

--- example/part1/Menu2ScreenUI.java
+++ example/part1/Menu2ScreenUI.java
@@ -3,6 +3,9 @@ public class Menu2ScreenUI implements ScreenUI {
         System.out.println("메뉴 2 화면으로 전환");
     }
     public void handleButton1Click(){
-         System.out.println("메뉴 2 화면의 버튼 처리");
+         System.out.println("메뉴 2 화면의 버튼1 처리");
+    }
+    public void handleButton2Click(){
+         System.out.println("메뉴 2 화면의 버튼2 처리");
     }
 }
