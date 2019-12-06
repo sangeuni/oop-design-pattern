8481dbb2a9c68aaf0d9bedc9f17b9f5d9a70e784
Sangeun Lee <goakgoak1995@gmail.com>
2019-12-06 21:18:24 +0900

    [list 2.1] example Member class
    list 2.1

---
+++ example/part1/Member.java
@@ -0,0 +1,12 @@
+public class Member {
+    ... // 다른 데이터
+	private Date expiryDate;
+    private boolean male;
+    
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+    public boolean isMale() {
+        return male;
+    }
+}
