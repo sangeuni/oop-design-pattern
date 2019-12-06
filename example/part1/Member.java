d5d9ee63d30b153ac456294ffccfa5e0fb743971
Sangeun Lee <goakgoak1995@gmail.com>
2019-12-06 21:22:09 +0900

    [list 2.3] modified Member class

--- example/part1/Member.java
+++ example/part1/Member.java
@@ -1,10 +1,15 @@
 public class Member {
+    private static final long DAY30 = 1000 * 60 * 60 * 24 * 30; // 30일
     ... // 다른 데이터
 	private Date expiryDate;
     private boolean male;
     
     public boolean isExpired() { // 만료 여부 확인 구현을 캡슐화
-        return expiryDate != null 
+        if(male){
+            return expiryDate != null 
             && expiryDate.getDate() < System.currentTimeMills();
+        }
+        return expiryDate != null 
+            && expiryDate.getDate() < System.currentTimeMills() - DAY30;
     }
 }
