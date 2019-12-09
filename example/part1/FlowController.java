8c90e51e69b66ae24693da122933dcee7beb7fea
Sangeun Lee <goakgoak1995@gmail.com>
2019-12-09 21:27:09 +0900

    Update FlowController.java
    [list 3.6]

--- example/part1/FlowController.java
+++ example/part1/FlowController.java
@@ -1,20 +1,8 @@
 public class FlowController {
     
-    private boolean useFile;
-    
-    public FlowController(boolean useFile) {
-        this.useFile = useFile;
-    }
-    
     public void process() {
-        byte[] data = null;
-        if (useFile) {
-            FileDataReader fileReader = new FileDataReader();
-       		data = fileReader.read();
-        }else {
-            SocketDataReader socketReader = new SocketDataReader();
-            data = socketReader.read();
-        }
+        ByteSource source = ByteSourceFactoy.getInstance().create();
+        byte[] data = source.read();       
         
         Encrytor encryptor = new Encryptor();
         byte[] encryptedData = encryptor.encrypt(data);
